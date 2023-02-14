0f9a3ee0a93152f71ae6f294d7504de059843d8f
Josefellype <rezefibr@gmail.com>
2023-02-14 16:40:27 -0300

    Código para impressão de todas as variaveis de tipo primitivo

---
+++ Varetip.java
@@ -0,0 +1,22 @@
+public class Varetip {
+    public static void main(String[] args){
+        byte a = 127;
+        short b = -32768;
+        int c = 21777;
+        long d = -777;
+        float e = 878755998;
+        double f = -2456;
+        char g = 3;
+        boolean h = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+
+}
